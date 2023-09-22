a9a90ed3795a62185761220a9a4bd84f2266889f
IsaacMartinez <isaamarti89@gmail.com>
2023-09-22 13:57:24 -0600

    Agregar archivos de proyecto.

---
+++ BL/Disco.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Disco
+    {
+        public ML.Result Add(ML.Disco disco)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+               using(DL.IMartinezPruebaTecnica2Entities1 context = new DL.IMartinezPruebaTecnica2Entities1())
+                {
+                    var query = context.DiscoAdd(disco.Titulo, disco.Artista, disco.GeneroMusical, disco.Duracion, disco.Año, disco.Distribuidora, disco.Ventas, disco.Disponibilidad);
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else 
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Error al ingresar el disco";
+                    }
+                    
+                } 
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage= ex.Message;
+                
+            }
+            return result;
+        }
+        public ML.Result Update(ML.Disco disco)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.IMartinezPruebaTecnica2Entities1 context = new DL.IMartinezPruebaTecnica2Entities1())
+                {
+                    var query = context.DiscoUpdate(disco.IdDico, disco.Titulo, disco.Artista, disco.GeneroMusical, disco.Duracion, disco.Año, disco.Distribuidora, disco.Ventas, disco.Disponibilidad);
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Error al actualizar el disco";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+
+            }
+            return result;
+        }
+
+        public ML.Result Delete (ML.Disco disco)
+        {
+            ML.Result result = new ML.Result ();
+            try
+            {
+                using (DL.IMartinezPruebaTecnica2Entities1 context = new DL.IMartinezPruebaTecnica2Entities1())
+                {
+                    var query = context.DiscoDelete(disco.IdDico);
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Error al eliminar el disco";
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage= ex.Message;
+            }
+            return result;
+        }
+    }
+}
---
+++ ML/Disco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Disco
+    {
+        public int IdDico { get; set; }
+        public string Titulo { get; set; }
+        public string Name { get; set; }
+        public string Artista { get; set; }
+        public string GeneroMusical { get; set; }
+        public string Duracion { get; set; }
+        public string Año { get; set; }
+        public string Distribuidora { get; set; }
+        public int Ventas { get; set; }
+        public string Disponibilidad { get; set; }
+        public List<object> Discos { get; set; }
+
+    }
+}
